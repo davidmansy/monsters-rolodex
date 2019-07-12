05c1d30ccce5730b745713da87ae2f62a0c7cbd1
David Mansy <david.mansy@icloud.com>
2019-07-12 14:33:53 +0200

    Updates

---
+++ precache-manifest.938597a2fd04545fc96942a53c86cc29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326a659171daa332a84d0dd68e594fae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "115223379a6facbd955c",
+    "url": "/monsters-rolodex/static/css/main.a404970d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "115223379a6facbd955c",
+    "url": "/monsters-rolodex/static/js/main.3e1ee701.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
